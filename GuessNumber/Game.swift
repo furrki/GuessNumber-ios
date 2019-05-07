0b86c8bbb3034161b7e85ff3cda2b90e7211bc49
Furkan Kaynar <furrki@korays-MacBook-Pro-2.local>
2019-05-08 00:50:44 +0300

    bugfix

--- GuessNumber/Game.swift
+++ GuessNumber/Game.swift
@@ -11,18 +11,24 @@ import Foundation
 class Game {
     var number: Int
     var tryCount = 0
+    var didFinish = false
     
     init() {
         number = Int.random(in: 1 ..< 100)
     }
     
     func hit(tryNumber: Int) -> String {
+        if didFinish {
+            return "Congratz! Found in \(tryCount) try."
+        }
+        
         tryCount += 1
         if tryNumber > number {
             return "Go Lower"
         } else if tryNumber < number {
             return "Go Upper"
         } else {
+            didFinish = true
             return "Congratz! Found in \(tryCount) try."
         }
     }
